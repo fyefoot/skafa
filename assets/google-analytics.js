456432a412d749afb08e0871387bd454ec3b5808
David Trott <haywra@outlook.com>
2026-01-02 18:57:08 +0200

    updated analytics

---
+++ assets/google-analytics.js
@@ -0,0 +1,26 @@
+(function () {
+  const isLocal =
+    location.hostname === 'localhost' ||
+    location.hostname === '127.0.0.1' ||
+    location.protocol === 'file:';
+
+    var fileName = (location.pathname === '/')? '/index.html' : location.pathname;
+    if (isLocal){
+      console.log('skafa is running locally - page name is \'' + fileName + '\'')
+    }
+    else {
+        // Load gtag.js
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = 'https://www.googletagmanager.com/gtag/js?id=G-6RGWLHBNBZ';
+        document.head.appendChild(script);
+
+        // Init GA
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        window.gtag = gtag;
+
+        gtag('js', new Date());
+        gtag('config', 'G-6RGWLHBNBZ');
+    }
+})();
